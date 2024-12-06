1ccf0dfd8b571b39fedf390dc2702c1db724374b
hoangtlm-dev <hoangtlm2410@gmail.com>
2024-12-06 13:59:15 +0700

    update middleware.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
+
+const fs = require("fs");
+const path = require("path");
+
+const publicDir = path.join(__dirname, "..", "public");
+
+if (!fs.existsSync(publicDir)) {
+  fs.mkdirSync(publicDir, { recursive: true });
+}
